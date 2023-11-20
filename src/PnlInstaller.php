97eef93a7090cd0e5d20624ca90ec62354783774
Fan2Shrek <pierre27.ambroise@gmail.com>
2023-11-20 22:16:23 +0100

    Update installer

--- src/PnlInstaller.php
+++ src/PnlInstaller.php
@@ -2,12 +2,17 @@
 
 namespace Pnl\PnlPhp;
 
-use Pnl\Installer\InstallerInterface;
+use Pnl\Installer\COR\InstallerInterface;
+use Pnl\Installer\PnlConfig;
 
 class PnlInstaller implements InstallerInterface
 {
-    public function install(string $gitlink)
+    public function install(PnlConfig $pnlConfig): PnlConfig
     {
         echo 'Installing Pnl...';
+
+        dd($pnlConfig);
+
+        return $pnlConfig;
     }
 }
