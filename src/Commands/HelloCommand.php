f0405c6f54e5dbf359a5a7e1486a5a53fe54da9b
Fan2Shrek <pierre27.ambroise@gmail.com>
2023-11-22 23:01:41 +0100

    :arrow_up: Update to new pnl/core version

---
+++ src/Commands/HelloCommand.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Pnl\PnlPhp\Commands;
+
+use Pnl\App\AbstractCommand;
+use Pnl\Console\Input\InputInterface;
+use Pnl\Console\Output\OutputInterface;
+
+class HelloCommand extends AbstractCommand
+{
+    private string $name = "hello";
+
+    public function getDescription(): string
+    {
+        return "Prints 'Hello, World!'";
+    }
+
+    public function __invoke(InputInterface $input, OutputInterface $output): void
+    {
+        $output->writeln("Hello, World!");
+    }
+}
---
+++ src/PNL.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Pnl\PnlPhp;
+use Pnl\Extensions\AbstractExtension;
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+
+class PNL extends AbstractExtension
+{
+    protected static string $name = "php";
+
+    public function loadServices(ContainerBuilder $container): void
+    {
+        $loader = new YamlFileLoader($container, new FileLocator(__DIR__ . '/../config'));
+        $loader->load('services.yaml');
+    }
+}
