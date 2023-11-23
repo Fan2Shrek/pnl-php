365c1c6893ade41ef8ed7e2712fd988c4b7a5962
Fan2Shrek <pierre27.ambroise@gmail.com>
2023-11-23 20:18:17 +0100

    Fully support pnl/core

--- src/PNL.php
+++ src/PNL.php
@@ -4,7 +4,6 @@ namespace Pnl\PnlPhp;
 use Pnl\Extensions\AbstractExtension;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 
 class PNL extends AbstractExtension
@@ -12,6 +11,17 @@ class PNL extends AbstractExtension
     protected static string $name = "php";
 
     public function loadServices(ContainerBuilder $container): void
+    {
+        $loader = new YamlFileLoader($container, new FileLocator(__DIR__ . '/../config'));
+        $loader->load('services.yaml');
+    }
+
+    public function getCommandTag(): string
+    {
+        return 'pnlphp-command';
+    }
+
+    public function prepareContainer(ContainerBuilder $container): void
     {
         $loader = new YamlFileLoader($container, new FileLocator(__DIR__ . '/../config'));
         $loader->load('services.yaml');
